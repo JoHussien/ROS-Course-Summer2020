e5f55530443edc0f6c8004e5dd42efe2ec57d687
JoHussien <youssefhussien@aucegypt.edu>
2020-08-12 17:20:02 +0200

    First Commit

---
+++ Day 1/Script/name.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<< "Hello My name is Youssef Hussien, aka JoCode, and I am trying to be a Software Engineer (ML engineer especifically) "<<endl; 
+}
---
+++ Day 1/readme.md
@@ -0,0 +1,3 @@
+This directory contains the tasls of the first week
+dir screenshots include the evidence of the working of each task
+dir script and dir images are as requested
