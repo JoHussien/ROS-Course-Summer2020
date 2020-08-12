4966ba5f358164d6f49bfd18b1639583997b70e0
JoHussien <youssefhussien@aucegypt.edu>
2020-08-12 23:45:14 +0200

    Second Task, running CPP file using CMAKE,screenshot task7 attached

---
+++ Day 2 /script/CMakeFiles/build/printing_name.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/build/printing_name.dir/src/name.cpp.o"
+  "build/printing_name.pdb"
+  "build/printing_name"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/build/printing_name.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
